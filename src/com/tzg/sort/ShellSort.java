57fd69d936eec217709744fa866e53c573288c9d
ZuguangTong <810732627@qq.com>
2020-06-03 12:06:06 +0800

    希尔排序移位法

--- src/com/tzg/sort/ShellSort.java
+++ src/com/tzg/sort/ShellSort.java
@@ -5,16 +5,15 @@ import java.util.Arrays;
 public class ShellSort {
     public static void main(String[] args) {
         int[] shellSortArray  = {3,2,4,6,5,9,-1,7,1,8};
-        shellSortExchange(shellSortArray);
+//        shellSortExchange(shellSortArray);
+        shellSortDisplace(shellSortArray);
         System.out.println(Arrays.toString(shellSortArray));
     }
     public static void shellSortExchange(int[] ssa){
         int temp = 0;
         for (int gap = ssa.length/2; gap >0 ; gap/=2) {
-            System.out.println("µÚÒ»²ã£º"+gap);
             for (int i = gap; i < ssa.length; i++) {
                 for (int j = i-gap; j >=0; j-=gap) {
-                    //
                     if (ssa[j]>ssa[j+gap]){
                         temp = ssa[j];
                         ssa[j] = ssa[j+gap];
@@ -26,7 +25,20 @@ public class ShellSort {
     }
 
 
-    public static void shellSortDisplace(int ssa){
+    public static void shellSortDisplace(int[] ssa){
+        for (int gap = ssa.length/2 ; gap >0 ; gap/=2) {
+            for (int i = gap; i < ssa.length; i++) {
 
+                int insertIndex = i;
+                int insertVal = ssa[insertIndex];
+                if (ssa[insertIndex]<ssa[insertIndex-gap]){
+                    while (insertIndex-gap>=0&&insertVal<ssa[insertIndex-gap]){
+                        ssa[insertIndex] = ssa[insertIndex-gap];
+                        insertIndex-=gap;
+                    }
+                    ssa[insertIndex] = insertVal;
+                }
+            }
+        }
     }
 }
