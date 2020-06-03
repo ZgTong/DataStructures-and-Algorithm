077d6c41dc19139a1081613a82e93c7cf6ad0b2d
ZuguangTong <810732627@qq.com>
2020-06-04 00:39:55 +0800

    快速排序

--- src/com/tzg/sort/QuickSort.java
+++ src/com/tzg/sort/QuickSort.java
@@ -1,5 +1,50 @@
 package com.tzg.sort;
 
-public class QuickSort {
+import java.util.Arrays;
 
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] quickSortArray = {-9,78,0,23,-567,70, -1,900, 4561};
+        quickSort(quickSortArray,0,quickSortArray.length-1);
+        System.out.println(Arrays.toString(quickSortArray));
+    }
+
+    public static void quickSort(int[] qsa,int left,int right){
+        int l = left;
+        int r = right;
+        int temp = 0;
+        int pivot = qsa[(left+right)/2];
+        while(l<r){
+            while ( qsa[l] < pivot ){
+                l+=1;
+            }
+            while ( qsa[r] > pivot ){
+                r-=1;
+            }
+            if (l>=r){
+                break;
+            }
+            temp = qsa[l];
+            qsa[l] = qsa[r];
+            qsa[r]= temp;
+            if (qsa[l]==pivot){
+                r-=1;
+            }
+            if (qsa[r]==pivot){
+                r+=1;
+            }
+        }
+        if (l == r){
+            l+=1;
+            r-=1;
+        }
+        if (left<r){
+            quickSort(qsa,left,r);
+        }
+        if (right>l){
+            quickSort(qsa,l,right);
+        }
+    }
 }
+
+
