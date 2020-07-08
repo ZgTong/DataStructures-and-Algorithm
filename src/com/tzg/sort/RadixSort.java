6bdc9ec54c3cf8c51bd15012bc8c13e03f12b881
ZuguangTong <810732627@qq.com>
2020-07-08 16:40:45 +0800

    基数排序复习

--- src/com/tzg/sort/RadixSort.java
+++ src/com/tzg/sort/RadixSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class RadixSort {
     public static void main(String[] args) {
         int radixSortArray[] = { 53, 3, 542, 748, 14, 214};
-        radixSort(radixSortArray);
+        radixSortReview(radixSortArray);
         System.out.println(Arrays.toString(radixSortArray));
     }
 
@@ -22,8 +22,6 @@ public class RadixSort {
 
         int[] bucketElementCount = new int[10];
         for (int i = 0, n =1; i < maxLength; i++,n*=10) {
-
-
             for (int j = 0; j <rsa.length ; j++) {
                 int digitOfElement = rsa[j]/n%10;
                 bucket[digitOfElement][bucketElementCount[digitOfElement]] = rsa[j];
@@ -34,6 +32,37 @@ public class RadixSort {
                 if (bucketElementCount[k]!=0){
                     for (int l = 0; l < bucketElementCount[k]; l++) {
                          rsa[index++]=bucket[k][l];
+                    }
+                }
+                bucketElementCount[k]=0;
+            }
+        }
+    }
+
+    public static void radixSortReview(int[] rsa){
+        int max = rsa[0];
+        for (int i = 0; i < rsa.length; i++) {
+            if (rsa[i]>max){
+                max = rsa[i];
+            }
+        }
+        int maxLength = (max+"").length();
+
+        int[][] bucket = new int[10][rsa.length];
+        int[] bucketElementCount = new int[10];
+
+        for (int i = 0,n=1; i <maxLength ; i++,n*=10) {
+            for (int j = 0; j < rsa.length; j++) {
+                int digitOfElement = rsa[j] / n %10;
+                bucket[digitOfElement][bucketElementCount[digitOfElement]] = rsa[j];
+                bucketElementCount[digitOfElement]++;
+            }
+
+            int index = 0;
+            for (int k = 0; k < bucketElementCount.length; k++) {
+                if (bucketElementCount[k]!=0){
+                    for (int l = 0; l < bucketElementCount[k] ; l++) {
+                        rsa[index++]= bucket[k][l];
                     }
                 }
                 bucketElementCount[k]=0;
