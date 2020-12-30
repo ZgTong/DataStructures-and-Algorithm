347a26e05e8c7787896455eac1bc4f5eab450f69
tongzuguang <tongzuguang_sx@qiyi.com>
2020-12-30 17:43:20 +0800

    init

--- src/com/tzg/queue/ArrayQueueDemo.java
+++ src/com/tzg/queue/ArrayQueueDemo.java
@@ -47,7 +47,6 @@ public class ArrayQueueDemo {
 					// TODO: handle exception
 					System.out.println(e.getMessage());
 				}
-				
 				break;
 			default:
 				break;
@@ -57,72 +56,69 @@ public class ArrayQueueDemo {
 	}
 }
 
-//使用数组模拟队列
-class ArrayQueue{
-	private int maxSize; //数组最大容量
-	private int front; // 队列头
-	private int rear; // 队列尾
-	private int[] arr; //该数组用于存放数据,模拟队列
-	public ArrayQueue(int arrMaxSize) {
+/**
+ * ArrayQueue
+ */	
+class ArrayQueue {
+	private int maxSize;
+	private int front; //指向第一个
+	private int rear; //指向最后一个
+	private int[] arr;
+	public ArrayQueue (int arrMaxSize) {
 		maxSize = arrMaxSize;
 		arr = new int[maxSize];
-		front = -1; //第一个数据的前一个位置
-		rear = -1; //指向队列尾的数据（即就是队列最后一个数据）
-		
+		front = 0;
+		rear = 0;
 	}
-	//判断队列是否已满
 	public boolean isFull() {
-		return rear == maxSize - 1;
+		return (rear + 1) % maxSize == front;
 	}
 	
-	//判断队列是否为空
 	public boolean isEmpty() {
-		return rear==front;
+		return rear == front;
 	}
 	
-	public void addQueue(int n){
-		//判断队列是否已满
+	public void addQueue(int n) {
+		// 判断队列是否满
 		if (isFull()) {
-			System.out.println("队列已满，不能加入数据");
+			System.out.println("队列满，不能加入数据~");
 			return;
 		}
-		rear++; //rear后移
 		arr[rear] = n;
-		
+		rear = (rear + 1) % maxSize;
 	}
-	
-	//获取队列的数据，出队列
+
 	public int getQueue() {
-		//判断队列是否为空
+		// 判断队列是否满
 		if (isEmpty()) {
-			//通过抛出异常
-			throw new RuntimeException("队列空 不能取数据");
+			throw new RuntimeException("队列空，不能取数据");
 		}
-		front++; //front 后移
+		int value = arr[front];
+		front = (front+1) % maxSize;
+		return value;
+	}
+
+	public void showQueue() {
+		// 遍历
+		if (isEmpty()) {
+			System.out.println("队列空的，没有数据~~");
+			return;
+		}
+		for (int i = front; i < front+size(); i++) {
+			System.out.printf("arr[%d]=%d\n", i % maxSize, arr[i % maxSize]);
+		}
+	}
+
+	public int size() {
+		return (rear-front+maxSize)%maxSize;
+	}
+
+	// 显示队列的头数据， 注意不是取出数据
+	public int headQueue() {
+		// 判断
+		if (isEmpty()) {
+			throw new RuntimeException("队列空的，没有数据~~");
+		}
 		return arr[front];
 	}
-	
-	//显示队列的所有数据
-	public void showQueue() {
-		if (isEmpty()) {
-			System.out.println("数列为空");
-		}
-		
-		for (int i = 0; i < arr.length; i++) {
-			System.out.printf("arr[%d]=%d\n",i,arr[i]);
-		}
-	}
-	
-	
-	//显示队列的头数据
-		public int headQueue() {
-			if (isEmpty()) {
-				throw new RuntimeException("数列为空");
-			}
-			
-			return arr[front+1];
-		}
-	
-	
-	
 }
